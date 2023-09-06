e92bf8866ea9c43b1a3bd7c1b447a6f20f900528
Tians-GH <122282490+Tians-GH@users.noreply.github.com>
2023-09-06 15:32:20 +0800

    接口文档分类

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -46,26 +45,46 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     }
 
     /**
-     * 通过knife4j生成接口文档
+     * 通过knife4j生成用户端接口文档
      *
      * @return
      */
     @Bean
-    public Docket docket() {
-        log.info("准备生成接口文档");
+    public Docket docket1() {
+        log.info("准备生成用户端接口文档...");
         ApiInfo apiInfo = new ApiInfoBuilder()
                 .title("苍穹外卖项目接口文档")
                 .version("2.0")
                 .description("苍穹外卖项目接口文档")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("用户端接口")
                 .apiInfo(apiInfo)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.sky.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.sky.controller.user")) // 指定生成接口需要扫描的包
                 .paths(PathSelectors.any())
                 .build();
         return docket;
     }
+
+    @Bean
+    public Docket docket2() {
+        log.info("准备生成管理端接口文档...");
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("苍穹外卖项目接口文档")
+                .version("2.0")
+                .description("苍穹外卖项目接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("管理端接口")
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sky.controller.admin"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+
 
     /**
      * 设置静态资源映射
@@ -85,13 +104,13 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      * @param converters
      */
     @Override
-    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         log.info("拓展消息转换器....");
         // 创建一个消息转换器
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         // 创建一个消息转换对象,消息转换对象可以将java对象序列化为JSON数据
         converter.setObjectMapper(new JacksonObjectMapper());
         // 把消息转换器增加到容器
-        converters.add(0,converter);
+        converters.add(0, converter);
     }
 }
